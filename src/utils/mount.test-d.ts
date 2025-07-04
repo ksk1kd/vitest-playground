3144037c6614eaff4d912dad94f4d8d6d8439202
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-07-05 00:16:26 +0900

    edit testing type

--- src/utils/mount.test-d.ts
+++ src/utils/mount.test-d.ts
@@ -1,8 +1,7 @@
-import { test } from 'vitest'
-import { expectTypeOf } from 'vitest'
+import { expectTypeOf, test } from 'vitest'
 import { mount } from './mount'
 
 test('my types work properly', () => {
   expectTypeOf(mount).toBeFunction()
-  expectTypeOf(mount).parameter(0).toMatchTypeOf<string>()
+  expectTypeOf(mount).parameter(0).toEqualTypeOf<number>()
 })
